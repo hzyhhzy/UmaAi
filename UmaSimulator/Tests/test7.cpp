91170b91746bbec159b4f5ed42b851be1e4c3f8b
hzy <2658628026@qq.com>
2023-07-30 16:27:19 +0800

    benchmark

---
+++ UmaSimulator/Tests/test7.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip> 
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <thread>  // for std::this_thread::sleep_for
+#include <chrono>  // for std::chrono::seconds
+
+#include "../Game/Game.h"
+#include "../GameDatabase/GameConfig.h"
+#include "../Search/Search.h"
+#include "windows.h"
+#include <filesystem>
+#include <cstdlib>
+using namespace std;
+
+inline int64_t now_ms()
+{
+  auto dur = std::chrono::steady_clock::now().time_since_epoch();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+}
+
+//benchmark
+void main_test7()
+{
+
+  const int threadNum = 1;
+  const double radicalFactor = 5;//激进度
+  const int searchN = 2048;
+
+
+  Search search;
+  vector<Evaluator> evaluators;
+  for (int i = 0; i < threadNum; i++)
+    evaluators.push_back(Evaluator(NULL, 128));
+
+
+
+
+
+
+  int umaId = 101101;//草上飞
+  int cards[6] = { 1,2,3,4,5,6 };
+
+  int zhongmaBlue[5] = { 18,0,0,0,0 };
+  int zhongmaBonus[6] = { 20,0,40,0,20,200 };
+
+  auto rand = mt19937_64(114514);
+  Game game;
+  game.newGame(rand, false, umaId, cards, zhongmaBlue, zhongmaBonus);
+  //game.print();
+
+  const int64_t time0 = now_ms();
+  search.runSearch(game, evaluators.data(), searchN, TOTAL_TURN, 27000, threadNum, radicalFactor);
+  const int64_t time1 = now_ms();
+
+  int64_t timeUsedMs = time1 - time0;
+  float speed = 1000 * searchN / float(timeUsedMs);
+  cout << "总搜索局数" << searchN << "，用时" << timeUsedMs << "ms，平均速度" << speed << "局每秒，线程数=" << threadNum << endl;
+  cout << "按任意键退出...";
+  cin.get();
+}
