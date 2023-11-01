5942ca43ad80372593363f00097837e5a75edb87
离披 <lipi@shuise.dev>
2023-11-02 06:25:05 +0800

    实现自动重连

--- UmaSimulator/websocket.h
+++ UmaSimulator/websocket.h
@@ -18,18 +18,16 @@ typedef websocketpp::client<websocketpp::config::asio_client> wsclient;
 class websocket {
 public:
 	websocket(std::string uri);
-
 	~websocket();
-
 	void on_open(wsclient* c, websocketpp::connection_hdl hdl);
-
 	void on_message(websocketpp::connection_hdl, wsclient::message_ptr msg);
-
+	void on_disconnect(wsclient* c, websocketpp::connection_hdl hdl);
 	void send(std::string message);
-
 	std::string get_status() const;
 private:
+	void connect();
 	wsclient m_endpoint;
+	std::string m_uri;
 	websocketpp::connection_hdl m_hdl;
 	websocketpp::lib::shared_ptr<websocketpp::lib::thread> m_thread;
 	std::string m_status;
