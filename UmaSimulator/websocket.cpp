fd689fd39c11b66fccad6e5c0259f60249adfd57
离披 <lipi@shuise.dev>
2023-11-02 06:25:05 +0800

    实现自动重连

--- UmaSimulator/websocket.cpp
+++ UmaSimulator/websocket.cpp
@@ -2,6 +2,7 @@
 std::string lastFromWs;
 websocket::websocket(std::string uri)
 	: m_status("Connecting")
+	, m_uri(uri)
 {
 	m_endpoint.clear_access_channels(websocketpp::log::alevel::all);
 	m_endpoint.clear_error_channels(websocketpp::log::elevel::all);
@@ -9,8 +10,44 @@ websocket::websocket(std::string uri)
 	m_endpoint.start_perpetual();
 	m_thread = websocketpp::lib::make_shared<websocketpp::lib::thread>(&wsclient::run, &m_endpoint);
 
+	connect();
+}
+websocket::~websocket() {
+	m_endpoint.stop_perpetual();
+
 	websocketpp::lib::error_code ec;
-	wsclient::connection_ptr con = m_endpoint.get_connection(uri, ec);
+	m_endpoint.close(m_hdl, websocketpp::close::status::going_away, "", ec);
+	if (ec) {
+		std::cout << "> Error closing connection: " << ec.message() << std::endl;
+	}
+
+	m_thread->join();
+}
+void websocket::on_open(wsclient* c, websocketpp::connection_hdl hdl) {
+	m_status = "Open";
+}
+void websocket::on_message(websocketpp::connection_hdl, wsclient::message_ptr msg) {
+	if (msg->get_opcode() == websocketpp::frame::opcode::text) {
+		const std::string json = msg->get_payload();
+		lastFromWs = json;
+	}
+	else {
+		std::cout << websocketpp::utility::to_hex(msg->get_payload()) << std::endl;
+	}
+}
+void websocket::on_disconnect(wsclient* c, websocketpp::connection_hdl hdl) {
+	m_status = "Lost";
+	auto con = c->get_con_from_hdl(hdl);
+	std::cout 
+		<< "与URA的websocket连接已断开："
+		<< con->get_ec().message()
+		<< std::endl;
+	connect();
+}
+void websocket::connect() {
+
+	websocketpp::lib::error_code ec;
+	wsclient::connection_ptr con = m_endpoint.get_connection(m_uri, ec);
 	m_hdl = con->get_handle();
 	if (ec) {
 		std::cout << "> Connect initialization error: " << ec.message() << std::endl;
@@ -23,54 +60,27 @@ websocket::websocket(std::string uri)
 		&m_endpoint,
 		websocketpp::lib::placeholders::_1
 	));
-	//con->set_fail_handler(websocketpp::lib::bind(
-	//	&websocket::on_fail,
-	//	wsptr,
-	//	&m_endpoint,
-	//	websocketpp::lib::placeholders::_1
-	//));
-	//con->set_close_handler(websocketpp::lib::bind(
-	//	&websocket::on_close,
-	//	wsptr,
-	//	&m_endpoint,
-	//	websocketpp::lib::placeholders::_1
-	//));
 	con->set_message_handler(websocketpp::lib::bind(
 		&websocket::on_message,
 		this,
 		websocketpp::lib::placeholders::_1,
 		websocketpp::lib::placeholders::_2
 	));
+	con->set_fail_handler(websocketpp::lib::bind(
+		&websocket::on_disconnect,
+		this,
+		&m_endpoint,
+		websocketpp::lib::placeholders::_1
+	));
+	con->set_close_handler(websocketpp::lib::bind(
+		&websocket::on_disconnect,
+		this,
+		&m_endpoint,
+		websocketpp::lib::placeholders::_1
+	));
 
 	m_endpoint.connect(con);
 }
-
-websocket::~websocket() {
-	m_endpoint.stop_perpetual();
-
-	websocketpp::lib::error_code ec;
-	m_endpoint.close(m_hdl, websocketpp::close::status::going_away, "", ec);
-	if (ec) {
-		std::cout << "> Error closing connection: " << ec.message() << std::endl;
-	}
-
-	m_thread->join();
-}
-
-void websocket::on_open(wsclient* c, websocketpp::connection_hdl hdl) {
-	m_status = "Open";
-}
-
-void websocket::on_message(websocketpp::connection_hdl, wsclient::message_ptr msg) {
-	if (msg->get_opcode() == websocketpp::frame::opcode::text) {
-		const std::string json = msg->get_payload();
-		lastFromWs = json;
-	}
-	else {
-		std::cout << websocketpp::utility::to_hex(msg->get_payload()) << std::endl;
-	}
-}
-
 void websocket::send(std::string message) {
 	websocketpp::lib::error_code ec;
 
@@ -80,7 +90,6 @@ void websocket::send(std::string message) {
 		return;
 	}
 }
-
 std::string websocket::get_status() const {
 	return m_status;
 }
