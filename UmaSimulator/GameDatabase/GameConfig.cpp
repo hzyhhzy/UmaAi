09c4ccac2cf2dff083a2b1f4855e70e08eb64806
hzy <2658628026@qq.com>
2023-12-06 19:18:55 +0800

    aiconfig里增加第二年凯旋门消debuff的设置，神经网络ai的设置

--- UmaSimulator/GameDatabase/GameConfig.cpp
+++ UmaSimulator/GameDatabase/GameConfig.cpp
@@ -4,14 +4,22 @@
 using namespace std;
 using json = nlohmann::json;
 
-bool GameConfig::noColor = false;
-double GameConfig::radicalFactor = 1;
+double GameConfig::radicalFactor = 5;
 int GameConfig::eventStrength = 20;
-int GameConfig::threadNum = 12;
-int GameConfig::searchN = 12288;
-bool GameConfig::debugPrint = false;
+bool GameConfig::removeDebuff5 = true;
+bool GameConfig::removeDebuff7 = true;
+
+string GameConfig::modelPath = "db/model_traced.pt";
+int GameConfig::threadNum = 4;
+int GameConfig::batchSize = 256;
+int GameConfig::searchN = 2000;
+int GameConfig::searchDepth = 10;
+
 bool GameConfig::useWebsocket = true;
 string GameConfig::role = "default";
+
+bool GameConfig::debugPrint = false;
+bool GameConfig::noColor = false;
 
 void GameConfig::load(const string& path)
 {
@@ -22,15 +30,23 @@ void GameConfig::load(const string& path)
     {
       // 创建默认配置JSON
       json j = {
-        {"noColor", GameConfig::noColor},
 				{"radicalFactor", GameConfig::radicalFactor},
 				{"eventStrength", GameConfig::eventStrength},
+				{"removeDebuff5", GameConfig::removeDebuff5},
+				{"removeDebuff7", GameConfig::removeDebuff7},
+
+				{"modelPath", GameConfig::modelPath},
         {"threadNum", GameConfig::threadNum},
-        {"searchN", GameConfig::searchN},
-        {"debugPrint", GameConfig::debugPrint},
+				{"batchSize", GameConfig::batchSize},
+				{"searchN", GameConfig::searchN},
+				{"searchDepth", GameConfig::searchDepth},
+
         {"useWebsocket", GameConfig::useWebsocket},
-        {"role", GameConfig::role}
-        };
+        {"role", GameConfig::role},
+
+				{"debugPrint", GameConfig::debugPrint},
+				{"noColor", GameConfig::noColor}
+      };
       // 写入
       ofstream ofs(path);
       ofs << j.dump(2);
@@ -45,16 +61,24 @@ void GameConfig::load(const string& path)
 		ifs.close();
 		json j = json::parse(ss.str(),nullptr,true,true);
 
-		j.at("noColor").get_to(GameConfig::noColor);
 		j.at("radicalFactor").get_to(GameConfig::radicalFactor);
 		j.at("eventStrength").get_to(GameConfig::eventStrength);
+		j.at("removeDebuff5").get_to(GameConfig::removeDebuff5);
+		j.at("removeDebuff7").get_to(GameConfig::removeDebuff7);
+
+
+		GameConfig::modelPath = j.value("modelPath", "db/model_traced.pt");
 		j.at("threadNum").get_to(GameConfig::threadNum);
+		j.at("batchSize").get_to(GameConfig::batchSize);
 		j.at("searchN").get_to(GameConfig::searchN);
-		j.at("debugPrint").get_to(GameConfig::debugPrint);
+		j.at("searchDepth").get_to(GameConfig::searchDepth);
+
 		j.at("useWebsocket").get_to(GameConfig::useWebsocket);
+		j.at("debugPrint").get_to(GameConfig::debugPrint);
+
+		j.at("noColor").get_to(GameConfig::noColor);
 		GameConfig::role = j.value("role", "default");
 
-		cout << GameConfig::radicalFactor << endl;
 		cout << "当前配置: " << j.dump(2) << endl;
 	}
 	catch (exception& e)
